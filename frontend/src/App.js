cb0904f66350e512a7829ff89a033bd106ddb1f9
VUNNAMNIRANJANKUMAR <128597004+VUNNAMNIRANJANKUMAR@users.noreply.github.com>
2026-02-06 14:28:58 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Flights from "./pages/Flights";
+import MyBookings from "./pages/MyBookings";
+
 
 export default function App() {
   return (
@@ -11,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Flights />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/mybookings" element={<MyBookings/>}/>
       </Routes>
     </BrowserRouter>
   );
