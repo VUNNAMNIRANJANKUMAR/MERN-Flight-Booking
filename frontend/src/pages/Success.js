9c8831fd9d37b244b8f1f9da7962c125382bdf33
VUNNAMNIRANJANKUMAR <128597004+VUNNAMNIRANJANKUMAR@users.noreply.github.com>
2026-02-07 21:18:01 +0530

    Create Success.js

---
+++ frontend/src/pages/Success.js
@@ -0,0 +1,8 @@
+export default function Success(){
+ return(
+  <div style={{textAlign:"center",marginTop:100}}>
+    <h1>✅ Booking Successful</h1>
+    <p>Your flight is booked.</p>
+  </div>
+ );
+}
