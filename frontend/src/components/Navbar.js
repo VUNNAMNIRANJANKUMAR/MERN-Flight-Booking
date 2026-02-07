25867fbba40804c188255d62ae98cff063fc3121
VUNNAMNIRANJANKUMAR <128597004+VUNNAMNIRANJANKUMAR@users.noreply.github.com>
2026-02-07 21:17:17 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,19 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function Navbar() {
-  return (
-    <div style={{ padding: 15, background: "#222", color: "white" }}>
-      <Link to="/" style={{ color: "white", marginRight: 15 }}>
-        Flights
-      </Link>
+export default function Navbar(){
 
-      <Link to="/mybookings" style={{ color: "white", marginRight: 15 }}>
-        My Bookings
-      </Link>
+ const navigate = useNavigate();
 
-      <Link to="/login" style={{ color: "white" }}>
-        Login
-      </Link>
-    </div>
-  );
+ const logout = ()=>{
+   localStorage.clear();
+   navigate("/login");
+ };
+
+ return(
+  <div style={{padding:15,background:"#222",color:"white"}}>
+
+    <Link to="/" style={{color:"white",marginRight:15}}>Flights</Link>
+    <Link to="/mybookings" style={{color:"white",marginRight:15}}>My Bookings</Link>
+
+    <button onClick={logout} style={{float:"right"}}>
+      Logout
+    </button>
+
+  </div>
+ );
 }
