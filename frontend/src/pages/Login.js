6e2baabc2c4305511d035246f1668199f0177ae1
VUNNAMNIRANJANKUMAR <128597004+VUNNAMNIRANJANKUMAR@users.noreply.github.com>
2026-02-07 18:39:41 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -13,7 +15,8 @@ export default function Login() {
     alert("Login success");
     console.log(res.data);
     localStorage.setItem("userId", res.data.user.id);
-    window.location.hash = "#/";
+    navigate("/");
+
 
 
   };
