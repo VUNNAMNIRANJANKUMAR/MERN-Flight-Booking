3656fc4e1cd4e27ba35a2124fe69e5a034af33d5
VUNNAMNIRANJANKUMAR <128597004+VUNNAMNIRANJANKUMAR@users.noreply.github.com>
2026-02-07 19:36:48 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Flights from "./pages/Flights";
 import MyBookings from "./pages/MyBookings";
 import Navbar from "./components/Navbar";
 
-
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Flights />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/mybookings" element={<MyBookings/>}/>
-  <BrowserRouter>
- <Navbar/>
- <Routes>
-
+        <Route path="/mybookings" element={<MyBookings />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
