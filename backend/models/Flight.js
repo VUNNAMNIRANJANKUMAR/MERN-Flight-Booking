408e158e7e7b2780d500c90d500361bca53be72a
VUNNAMNIRANJANKUMAR <128597004+VUNNAMNIRANJANKUMAR@users.noreply.github.com>
2026-02-09 19:42:39 +0530

    Update Flight.js

--- backend/models/Flight.js
+++ backend/models/Flight.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
 const flightSchema = new mongoose.Schema({
-  from: String,
-  to: String,
-  journeyDate: String,
-  departureTime: String,
-  arrivalTime: String,
-  price: Number,
-  seats: Number,
-  airline: String,
-travelClass: String
-}, { timestamps: true });
+ from:String,
+ to:String,
+ journeyDate:String,
+ departureTime:String,
+ arrivalTime:String,
+ price:Number,
+ seats:Number,
+ airline:String,
+ travelClass:String
+},{timestamps:true});
+
+module.exports = mongoose.model("Flight",flightSchema);
