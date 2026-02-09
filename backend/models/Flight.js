7595e40f53e0567dfb2b9f45b368ba0f699ad9a1
VUNNAMNIRANJANKUMAR <128597004+VUNNAMNIRANJANKUMAR@users.noreply.github.com>
2026-02-09 19:35:44 +0530

    Update Flight.js

--- backend/models/Flight.js
+++ backend/models/Flight.js
@@ -13,3 +13,15 @@ travelClass: String
 }, { timestamps: true });
 
 module.exports = mongoose.model("Flight", flightSchema);
+
+router.post("/add", async (req,res)=>{
+ try{
+
+  const flight = await Flight.create(req.body);
+
+  res.json(flight);
+
+ }catch(err){
+  res.status(500).json({error:err.message});
+ }
+});
