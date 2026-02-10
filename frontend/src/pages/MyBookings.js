3a32633544ee9c3e82bd4df20f29d0787798e6a1
VUNNAMNIRANJANKUMAR <128597004+VUNNAMNIRANJANKUMAR@users.noreply.github.com>
2026-02-10 21:46:23 +0530

    Update MyBookings.js

--- frontend/src/pages/MyBookings.js
+++ frontend/src/pages/MyBookings.js
@@ -8,31 +8,33 @@ export default function MyBookings(){
  const [bookings,setBookings]=useState([]);
 
  useEffect(()=>{
-  const user=JSON.parse(localStorage.getItem("user"));
 
-  if(!user){
-   alert("Please login");
-   return;
-  }
+  const user=JSON.parse(localStorage.getItem("user"));
+  if(!user) return;
 
   axios.get(`${API}/api/booking/${user._id}`)
-  .then(res=>setBookings(res.data))
-  .catch(err=>console.log(err));
+   .then(res=>setBookings(res.data))
+   .catch(err=>console.log(err));
 
  },[]);
 
  return(
   <div style={{padding:20}}>
+
    <h2>My Bookings</h2>
 
-   {bookings.length===0 && <p>No bookings yet</p>}
+   {bookings.length===0 && <p>No bookings</p>}
 
    {bookings.map(b=>(
-    <div key={b._id} style={{border:"1px solid #ccc",padding:10,margin:10}}>
+    <div key={b._id} style={{border:"1px solid",margin:10,padding:10}}>
+
      <h3>{b.flightId?.from} → {b.flightId?.to}</h3>
+
      <p>Date: {b.flightId?.journeyDate}</p>
      <p>Departure: {b.flightId?.departureTime}</p>
-     <p>Status: {new Date(b.flightId?.journeyDate) < new Date() ? "Journey Over" : "Yet To Fly"}</p>
+
+     <p>Status: {b.status}</p>
+
     </div>
    ))}
 
