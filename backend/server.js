187dd599c75cbc8149d0cf61284bef2ceac091b6
VUNNAMNIRANJANKUMAR <128597004+VUNNAMNIRANJANKUMAR@users.noreply.github.com>
2026-02-10 21:14:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
 const flightRoutes = require("./routes/flight");
-const bookingRoutes = require("./routes/booking");
+const bookingRoutes = require("./routes/bookings");
 
 require("dotenv").config();
 
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(express.json());
 app.use("/api/auth",authRoutes);
 app.use("/api/flight",flightRoutes);
-app.use("/api/booking",bookingRoutes);
+app.use("/api/bookings",bookingRoutes);
 
 
 app.get("/", (req, res) => {
