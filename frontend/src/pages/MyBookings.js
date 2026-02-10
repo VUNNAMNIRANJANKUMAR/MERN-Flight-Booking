0f10b50aa50dad29c0450e396b9f07131fed6f26
VUNNAMNIRANJANKUMAR <128597004+VUNNAMNIRANJANKUMAR@users.noreply.github.com>
2026-02-10 21:27:25 +0530

    Update MyBookings.js

--- frontend/src/pages/MyBookings.js
+++ frontend/src/pages/MyBookings.js
@@ -15,7 +15,7 @@ export default function MyBookings(){
    return;
   }
 
-  axios.get(`${API}/api/bookings/user/${user._id}`)
+  axios.get(`${API}/api/booking/user/${user._id}`)
   .then(res=>setBookings(res.data))
   .catch(err=>console.log(err));
 
