c515999ebf44f6e8d8126422c43f775a5311144f
VUNNAMNIRANJANKUMAR <128597004+VUNNAMNIRANJANKUMAR@users.noreply.github.com>
2026-04-05 00:00:22 +0530

    Update Booking.js

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -1,15 +1,27 @@
 const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  flightId: { type: mongoose.Schema.Types.ObjectId, ref: "Flight", required: true },
-  seatsBooked: { type: Number, required: true },
-  totalPrice: { type: Number, required: true },
-  status: { type: String, default: "Booked" } // Booked, Cancelled
+  userId: {
+    type: String,
+    required: true
+  },
+  flightId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Flight"
+  },
+  seatsBooked: Number,
+  totalPrice: Number,
+
+  status: {
+    type: String,
+    default: "Booked"
+  }, // ✅ comma added here
+
   pnr: {
-  type: String,
-  unique: true
-}
+    type: String,
+    unique: true
+  }
+
 }, { timestamps: true });
 
 module.exports = mongoose.model("Booking", bookingSchema);
